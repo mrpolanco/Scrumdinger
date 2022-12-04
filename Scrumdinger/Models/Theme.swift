1d3ca03e5331b8cdede77f0de665347e874e0873
Rich Polanco <richardpolanco@gmail.com>
2022-12-04 07:06:10 -0600

    Theme colors

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,34 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by Rich Polanco on 12/4/22.
+//
+
+import SwiftUI
+
+enum Theme: String {
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+}
